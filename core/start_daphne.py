3171223f1638c658330eb7632b1e6515fd14aef4
AmalJose664 <amal446446@gmail.com>
2025-04-27 01:10:34 +0530

    Port  changed.. more

--- core/start_daphne.py
+++ core/start_daphne.py
@@ -7,4 +7,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 from daphne import server, cli
 
 if __name__ == "__main__":
-    cli.CommandLineInterface().run(["core.asgi:application"])
+    cli.CommandLineInterface().run(["core.asgi:application", "--port", "8000",
+        "--bind", "0.0.0.0"])
