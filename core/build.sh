65ae424d427138433b211fc9f6661a7fa7578821
AmalJose664 <amal446446@gmail.com>
2025-04-27 00:19:13 +0530

    Last changes.. more

--- core/build.sh
+++ core/build.sh
@@ -3,7 +3,7 @@ python3 -m venv venv
 
 # Activate the virtual environment
 source venv/bin/activate
-cd core
-pip install -r ./requirements.txt
 
-python manage.py collectstatic --no-input
+pip install -r ./core/requirements.txt
+
+python ./core/manage.py collectstatic --no-input
