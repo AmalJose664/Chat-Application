7a90acaee5c403f91312fae1bc8b409ad27c7725
AmalJose664 <amal446446@gmail.com>
2025-04-28 19:53:47 +0530

    vite changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server:{
 	  allowedHosts: [
-		  '7e37-2403-a080-410-e260-9c5e-7ab3-def7-98af.ngrok-free.app',
-		  '192.168.20.3'
 	  ],
 	  host: true,
   }
