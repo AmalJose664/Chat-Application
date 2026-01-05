9b99e417d62b90b14795e702f11b81451cd67e9d
Amal Jose <amal446446@gmail.com>
2026-01-05 16:42:27 +0530

    fix: login bg fix

--- frontend/src/components/HomePage/HomepageHelpers/ListUsers.jsx
+++ frontend/src/components/HomePage/HomepageHelpers/ListUsers.jsx
@@ -1,119 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import './ListUsers.css'
-import { userDataStore } from '../../../store/userDataStore.js'
-import { motion } from "framer-motion";
-import { axiosMessageInstance } from '../../../lib/axios.js';
-import { filterLastMessage, lastMessage } from '../../../lib/lastMessageFilter.js';
-import { useSpecialStore } from '../../../store/specialStore.js';
-import ReadReceipts from '../../../assets/ReadReceipts.jsx';
-import { useAuthStore } from '../../../store/useAuthStore.js';
-import { getTime } from '../../../lib/timefilter.js';
-import { useChatStore } from '../../../store/useChatStore.js';
-import { useGroupConnectStore } from '../../../store/useGroupConnect.js';
-import Refresh from '../../../assets/Refresh.jsx';
-import { toast } from 'sonner';
-
-
+import React, { useEffect, useState } from "react"
+import "./ListUsers.css"
+import { userDataStore } from "../../../store/userDataStore.js"
+import { motion } from "framer-motion"
+import { axiosMessageInstance } from "../../../lib/axios.js"
+import { filterLastMessage, lastMessage } from "../../../lib/lastMessageFilter.js"
+import { useSpecialStore } from "../../../store/specialStore.js"
+import ReadReceipts from "../../../assets/ReadReceipts.jsx"
+import { useAuthStore } from "../../../store/useAuthStore.js"
+import { getTime } from "../../../lib/timefilter.js"
+import { useChatStore } from "../../../store/useChatStore.js"
+import { useGroupConnectStore } from "../../../store/useGroupConnect.js"
+import Refresh from "../../../assets/Refresh.jsx"
+import { toast } from "sonner"
 
 function ListUsers() {
-	
-	const convListTyping = useSpecialStore(state => state.convListTyping)
-	const conversations = userDataStore(state => state.conversations)
-	const setConversationsStore = userDataStore(state => state.setConversationsStore)
-	const selectedUserId = userDataStore(state => state.selectedUser?.sqlId)
-	const deleteSocket = useChatStore((state) => state.deleteSocket);
-	const deleteSocketGroup = useGroupConnectStore(state => state.deleteSocket)
+	const convListTyping = useSpecialStore((state) => state.convListTyping)
+	const conversations = userDataStore((state) => state.conversations)
+	const setConversationsStore = userDataStore((state) => state.setConversationsStore)
+	const selectedUserId = userDataStore((state) => state.selectedUser?.sqlId)
+	const deleteSocket = useChatStore((state) => state.deleteSocket)
+	const deleteSocketGroup = useGroupConnectStore((state) => state.deleteSocket)
 
-	const usersOnline = useSpecialStore(state => state.usersOnline)
-	const [convLoader,setConvLoader] = useState(false)
+	const usersOnline = useSpecialStore((state) => state.usersOnline)
+	const [convLoader, setConvLoader] = useState(false)
 
-	const getConversations = async()=>{
+	const getConversations = async () => {
 		setConvLoader(true)
-		try{
-			const response = await axiosMessageInstance.get('conversations/?limit=30&cursor=0000000')
+		try {
+			const response = await axiosMessageInstance.get("conversations/?limit=30&cursor=0000000")
 
 			setConversationsStore(response.data.conversations, response.data.unseen_noti)
-			
-		}catch(err){
-			console.log(err, err.message);
-			toast.error("Error on loading conversation !! "+ err.message )
-			
-		}finally{
+		} catch (err) {
+			console.log(err, err.message)
+			toast.error("Error on loading conversation !! " + err.message)
+		} finally {
 			setConvLoader(false)
 		}
-
 	}
-	 
-	
-	
-	useEffect(()=>{
+
+	useEffect(() => {
 		deleteSocketGroup()
-		if(conversations.length==0){
+		if (conversations.length == 0) {
 			getConversations()
 		}
-		return ()=>{
+		return () => {
 			deleteSocket()
 		}
-	},[])
-	
-  return (
-	<div className='home-users-container'>
-		  <div className="home-refresh users" onClick={() => {setConversationsStore([],0);getConversations()}}>
-			<Refresh color='white'/>
+	}, [])
+
+	return (
+		<div className="home-users-container">
+			<div
+				className="home-refresh users"
+				onClick={() => {
+					setConversationsStore([], 0)
+					getConversations()
+				}}
+			>
+				<Refresh color="white" />
+			</div>
+			<div className="home-users">
+				{convLoader && (
+					<motion.div className="list-friends-loader" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }}>
+						<l-tail-chase size="28" speed="1.8" color="white"></l-tail-chase>
+					</motion.div>
+				)}
+				{conversations.length != 0
+					? conversations.map((value, i) => {
+							if (value.other_user) {
+								return (
+									<div
+										key={i}
+										className={value.other_user.sqlite_id == selectedUserId ? "home-list-users-each selected" : "home-list-users-each"}
+									>
+										<EachConversations
+											value={value}
+											i={i}
+											key={i}
+											type={!!convListTyping[value.other_user.sqlite_id]}
+											isOnline={!!usersOnline[value.other_user.sqlite_id]}
+										/>
+									</div>
+								)
+							} else return ""
+					  })
+					: !convLoader && (
+							<motion.p initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} className="">
+								Start a new Chat
+							</motion.p>
+					  )}
+			</div>
 		</div>
-		<div className="home-users">
-			  {convLoader && <motion.div className='list-friends-loader' initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, }}>
-				  < l-tail-chase
-					  size="28"
-					  speed="1.8"
-					  color="white"
-				  ></l-tail-chase >
-			  </motion.div>}
-			{( conversations.length != 0 ) ? (conversations.map((value, i) => {
-				if(value.other_user) {
-					return (
-						<div key={i} className={value.other_user.sqlite_id == selectedUserId ? "home-list-users-each selected" : "home-list-users-each"}>
-							<EachConversations value={value} i={i} key={i} type={!!convListTyping[value.other_user.sqlite_id]}
-								isOnline={!!usersOnline[value.other_user.sqlite_id]} />
-						</div>
-							
-					)
-				}else return ""
-				
-			})) : !convLoader && <motion.p initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, }}
-				className=''>Start a new Chat</motion.p> } 
-			
-		</div>
-		
-		
-	</div>
-	
-  )
+	)
 }
 
-const EachConversations = React.memo(({ value, i: index, type, isOnline,  })=>{
+const EachConversations = React.memo(({ value, i: index, type, isOnline }) => {
+	const authUser = useAuthStore((state) => state.authUser)
+	const clearUnread = userDataStore((state) => state.clearUnread)
 
-	
-	const authUser = useAuthStore(state => state.authUser)
-	const clearUnread = userDataStore(state => state.clearUnread)
-	
-	
-	const setSelectUser = userDataStore(state => state.setSelectUser)
-	const getSocketDetails = useChatStore(state => state.getSocketDetails)
-	const changeRoomOrUser = useChatStore(state => state.changeRoomOrUser)
-	const setTempSelectedUser = userDataStore(state => state.setTempSelectedUser)
-	const changingRoom = useChatStore((state) => state.changingRoom);
-	
+	const setSelectUser = userDataStore((state) => state.setSelectUser)
+	const getSocketDetails = useChatStore((state) => state.getSocketDetails)
+	const changeRoomOrUser = useChatStore((state) => state.changeRoomOrUser)
+	const setTempSelectedUser = userDataStore((state) => state.setTempSelectedUser)
+	const changingRoom = useChatStore((state) => state.changingRoom)
 
-	const setUserPictData = useSpecialStore(state => state.setUserPictData)
+	const setUserPictData = useSpecialStore((state) => state.setUserPictData)
 
-	
 	const changeUser = (value, i, unread) => {
 		const readyState = getSocketDetails()
-		if (readyState == 0 || changingRoom){
+		if (readyState == 0 || changingRoom) {
 			return
-		}
-		else if (readyState != 1) {
+		} else if (readyState != 1) {
 			setSelectUser({
 				user_name: value.other_user.user_name,
 				id: value.other_user._id,
@@ -121,10 +118,9 @@ const EachConversations = React.memo(({ value, i: index, type, isOnline,  })=>{
 				user_id: value.other_user._id,
 				pic: value.other_user.profile_picture,
 				conv_v: i,
-				conv_id: value._id
+				conv_id: value._id,
 			})
-		}
-		else{
+		} else {
 			setTempSelectedUser({
 				user_name: value.other_user.user_name,
 				id: value.other_user._id,
@@ -132,72 +128,69 @@ const EachConversations = React.memo(({ value, i: index, type, isOnline,  })=>{
 				user_id: value.other_user._id,
 				pic: value.other_user.profile_picture,
 				conv_v: i,
-				conv_id: value._id
+				conv_id: value._id,
 			})
 			changeRoomOrUser(value.other_user.sqlite_id)
 		}
-		
-		
+
 		if (unread > 0) {
 			clearUnread(i)
 		}
 	}
-	const showMyPic = (obj)=>{	
+	const showMyPic = (obj) => {
 		setUserPictData(obj)
 	}
 
-
 	return (
-
-		<motion.div key={index} className={value.unread_count != 0 ? "home-user unread" : "home-user"}
-			initial={{ opacity: 0, x: -60, }} animate={{ opacity: 1, x: 0, }}
-			transition={{ duration: 0.5, delay: index / 10, ease: 'easeInOut', }}>
-
-			<div className='home-user-cont' >
-				<div className={isOnline ? "home-user-pic is-online" : "home-user-pic"} 
-					onClick={() => showMyPic(value.other_user )}>
+		<motion.div
+			key={index}
+			className={value.unread_count != 0 ? "home-user unread" : "home-user"}
+			initial={{ opacity: 0, x: -60 }}
+			animate={{ opacity: 1, x: 0 }}
+			transition={{ duration: 0.5, delay: index / 10, ease: "easeInOut" }}
+		>
+			<div className="home-user-cont">
+				<div className={isOnline ? "home-user-pic is-online" : "home-user-pic"} onClick={() => showMyPic(value.other_user)}>
 					<img src={value.other_user.profile_picture} alt="" />
 				</div>
-				<div className="home-user-detail"
-					onClick={() => changeUser(value, index, value.unread_count)}>
+				<div className="home-user-detail" onClick={() => changeUser(value, index, value.unread_count)}>
 					<div className="user-top">
-						<div className="user-title">
-							{value.other_user.user_name}
-						</div>
-						{value.unread_count != 0 && <div className="user-unread">
-							{value.unread_count}
-						</div>}
+						<div className="user-title">{value.other_user.user_name}</div>
+						{value.unread_count != 0 && <div className="user-unread">{value.unread_count}</div>}
 					</div>
 					<div className="user-bottom">
 						<div className="user-last-m">
 							{/* {filterLastMessage(value.lst_m, authUser.db_user._id) ? 'last message by me' : "lastmessage by other user"} */}
-							{type 	? 
-							<motion.div className="list-covns-user-istyping" initial={{ opacity: 0, y: 20, }} animate={{ opacity: 1, y: 0, }}
-								transition={{ duration: 0.2}}>
-								<l-leapfrog
-									size="20"
-									speed="2"
-									color="#78c5f4"
-								></l-leapfrog>
-							</motion.div> 
-							: 
-							<motion.span initial={{ opacity: 0, y: 20, }} animate={{ opacity: 1, y: 0, }}
-								transition={{ duration: 0.2 }} >{lastMessage(value.lst_m)}</motion.span>
-							}
+							{type ? (
+								<motion.div
+									className="list-covns-user-istyping"
+									initial={{ opacity: 0, y: 20 }}
+									animate={{ opacity: 1, y: 0 }}
+									transition={{ duration: 0.2 }}
+								>
+									<l-leapfrog size="20" speed="2" color="#78c5f4"></l-leapfrog>
+								</motion.div>
+							) : (
+								<motion.span initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }}>
+									{lastMessage(value.lst_m)}
+								</motion.span>
+							)}
 						</div>
 						<div className="user-last-status">
-							<div className="show-time-user-list-conv-list">
-								{getTime(value.t,false)}
-							</div>
-							{filterLastMessage(value.lst_m, authUser.db_user._id) ? <><ReadReceipts status={value.l_s} />
-								{/* <p>{value.l_s}</p> */}
-								</> : ""}
+							<div className="show-time-user-list-conv-list">{getTime(value.t, false)}</div>
+							{filterLastMessage(value.lst_m, authUser.db_user._id) ? (
+								<>
+									<ReadReceipts status={value.l_s} />
+									{/* <p>{value.l_s}</p> */}
+								</>
+							) : (
+								""
+							)}
 						</div>
 					</div>
 				</div>
 			</div>
 		</motion.div>
-	
 	)
 })
 
--- frontend/src/components/Login/LoginComp.jsx
+++ frontend/src/components/Login/LoginComp.jsx
@@ -1,49 +1,53 @@
-import React, {  useState } from 'react'
-import './LoginComp.css'
-import '/32947939_491362538070.jpg'
-import {EyeFill, EyeIcon} from '../../assets/eye.jsx'
-import Dybtn from '../../assets/Dybtn.jsx'
-import Logo from '../../assets/Logo.jsx'
-import { useAuthStore } from '../../store/useAuthStore.js'
-import {Link } from 'react-router-dom'
-import {motion} from 'framer-motion'
-import { APP_Name } from '../../lib/chatUtilities.js';
-
-
+import React, { useState } from "react"
+import "./LoginComp.css"
+import "/32947939_491362538070.jpg"
+import { EyeFill, EyeIcon } from "../../assets/eye.jsx"
+import Dybtn from "../../assets/Dybtn.jsx"
+import Logo from "../../assets/Logo.jsx"
+import { useAuthStore } from "../../store/useAuthStore.js"
+import { Link } from "react-router-dom"
+import { motion } from "framer-motion"
+import { APP_Name } from "../../lib/chatUtilities.js"
 
 function LoginComp() {
-	
-	
 	const { login } = useAuthStore()
 	const [passShow, setPassShow] = useState(false)
 	const [formData, setForm] = useState({
-		email: '',
-		password: '',
+		email: "",
+		password: "",
 	})
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 		await login(formData)
-		
 	}
 
 	return (
-	<div className='login-component'>
+		<div className="login-component">
 			<div className="content">
 				<div className="text">
-					<h1>{APP_Name} <Logo size={30} /> </h1>
+					<h1>
+						{APP_Name} <Logo size={30} />{" "}
+					</h1>
 				</div>
 				<div className="power-text">
-					<h1>By {APP_Name}<Logo size={30} /></h1>
+					<h1>
+						By {APP_Name}
+						<Logo size={30} />
+					</h1>
 				</div>
-				<motion.div className="left" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, }}>
-					
+				<motion.div
+					className="left"
+					style={{ backgroundImage: "url('/32947939_491362538070.jpg')", backgroundPosition: "center", backgroundSize: "cover" }}
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ duration: 0.2 }}
+				>
 					<div className="grid">
-						<GridView/>
+						<GridView />
 					</div>
 				</motion.div>
-				<motion.div className="right-part" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, delay:.5}}>
-					
-					<motion.div className="right lines" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, delay:1 }}>
+				<motion.div className="right-part" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, delay: 0.5 }}>
+					<motion.div className="right lines" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, delay: 1 }}>
 						<span></span>
 						<span></span>
 						<span></span>
@@ -51,28 +55,48 @@ function LoginComp() {
 						<h1>Log In</h1>
 
 						<div className="form-content">
-							< form onSubmit={handleSubmit} >
+							<form onSubmit={handleSubmit}>
 								<div className="input">
-									<motion.input required type="email" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, delay: 1.2 }}
+									<motion.input
+										required
+										type="email"
+										initial={{ opacity: 0, y: 20 }}
+										animate={{ opacity: 1, y: 0 }}
+										transition={{ duration: 0.2, delay: 1.2 }}
 										value={formData.email}
-										onChange={(e) => setForm({ ...formData, email: e.target.value })} />
-									<label htmlFor="password">
-										Email
-									</label>
+										onChange={(e) => setForm({ ...formData, email: e.target.value })}
+									/>
+									<label htmlFor="password">Email</label>
 								</div>
 								<div className="input">
-									<motion.input required type={passShow ? 'text' : 'password'} initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, delay: 1.2 }}
+									<motion.input
+										required
+										type={passShow ? "text" : "password"}
+										initial={{ opacity: 0, y: 20 }}
+										animate={{ opacity: 1, y: 0 }}
+										transition={{ duration: 0.2, delay: 1.2 }}
 										value={formData.password}
-										onChange={(e) => setForm({ ...formData, password: e.target.value })} />
+										onChange={(e) => setForm({ ...formData, password: e.target.value })}
+									/>
 									<label htmlFor="">Password</label>
-									<span type="button" onClick={() => setPassShow(!passShow)}> {passShow ? <EyeFill size={20} color="white" /> : <EyeIcon size={20} color="black" />}   </span>
+									<span type="button" onClick={() => setPassShow(!passShow)}>
+										{" "}
+										{passShow ? <EyeFill size={20} color="white" /> : <EyeIcon size={20} color="black" />}{" "}
+									</span>
 								</div>
-								<motion.div className="input" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, delay: 1.3 }}>
-									<Dybtn text='sign in' />
-									
+								<motion.div
+									className="input"
+									initial={{ opacity: 0, y: 20 }}
+									animate={{ opacity: 1, y: 0 }}
+									transition={{ duration: 0.2, delay: 1.3 }}
+								>
+									<Dybtn text="sign in" />
 								</motion.div>
-							</form >
-						</div><Link to="/create" className="to-login"><p className='to-login' >Create new chat account</p></Link>
+							</form>
+						</div>
+						<Link to="/create" className="to-login">
+							<p className="to-login">Create new chat account</p>
+						</Link>
 					</motion.div>
 				</motion.div>
 			</div>
@@ -81,47 +105,39 @@ function LoginComp() {
 }
 export default LoginComp
 
-function GridView(){
-	const boxes = Array.from({ length: 40 });
+function GridView() {
+	const boxes = Array.from({ length: 40 })
 	return (
-		<motion.div className="grid-content" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2,delay:.4 }}>
+		<motion.div className="grid-content" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2, delay: 0.4 }}>
 			{boxes.map((_, index) => {
-					if (index == 17) {
-						return (
-							<div key={index} className="box b-content">
-								<div className="inner-box">
-									<h3>Share Your Smile</h3>
-									<svg
-										width="100"
-										height="100"
-										viewBox="0 0 24 24"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										
-										<path
-											d="M4 16V5C4 3.9 4.9 3 6 3H18C19.1 3 20 3.9 20 5V13C20 14.1 19.1 15 17 15H8L4 16"
-											stroke="#fff"
-											strokeWidth="2"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-										/>
-										
-										<circle cx="9" cy="9" r="1.2" fill="#fff" />
-										<circle cx="12" cy="9" r="1.2" fill="#fff" />
-										<circle cx="15" cy="9" r="1.2" fill="#fff" />
-									</svg>
+				if (index == 17) {
+					return (
+						<div key={index} className="box b-content">
+							<div className="inner-box">
+								<h3>Share Your Smile</h3>
+								<svg width="100" height="100" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path
+										d="M4 16V5C4 3.9 4.9 3 6 3H18C19.1 3 20 3.9 20 5V13C20 14.1 19.1 15 17 15H8L4 16"
+										stroke="#fff"
+										strokeWidth="2"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+									/>
 
-								</div>
+									<circle cx="9" cy="9" r="1.2" fill="#fff" />
+									<circle cx="12" cy="9" r="1.2" fill="#fff" />
+									<circle cx="15" cy="9" r="1.2" fill="#fff" />
+								</svg>
 							</div>
-						)
-					}
+						</div>
+					)
+				}
 				if (index == 22) {
 					return (
 						<div key={index} className="box b-content">
 							<div className="inner-box">
 								<h3>Your words, your vibe, your community!</h3>
-								<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#fff"  viewBox="0 0 16 16">
+								<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#fff" viewBox="0 0 16 16">
 									<path d="M6 7.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m4.5.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m-.5 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
 									<path d="M8 0a7.96 7.96 0 0 0-4.075 1.114q-.245.102-.437.28A8 8 0 1 0 8 0m3.25 14.201a1.5 1.5 0 0 0-2.13.71A7 7 0 0 1 8 15a6.97 6.97 0 0 1-3.845-1.15 1.5 1.5 0 1 0-2.005-2.005A6.97 6.97 0 0 1 1 8c0-1.953.8-3.719 2.09-4.989a1.5 1.5 0 1 0 2.469-1.574A7 7 0 0 1 8 1c1.42 0 2.742.423 3.845 1.15a1.5 1.5 0 1 0 2.005 2.005A6.97 6.97 0 0 1 15 8c0 .596-.074 1.174-.214 1.727a1.5 1.5 0 1 0-1.025 2.25 7 7 0 0 1-2.51 2.224Z" />
 								</svg>
@@ -142,11 +158,8 @@ function GridView(){
 						</div>
 					)
 				}
-					return (
-						<div key={index} className="box"></div>
-					)})
-			}
+				return <div key={index} className="box"></div>
+			})}
 		</motion.div>
-		)
+	)
 }
-
